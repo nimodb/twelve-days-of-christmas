9b0a5747c59617c7a75e22df3127d57fe08977f7
Nima Saadati <nimodbdev@gmail.com>
2024-08-26 12:49:08 +0400

    Add function to print placeholder for each day of the song

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Twelve Days of Christmas Song");
+    print_lyrics();
+}
+
+fn print_lyrics() {
+    for day in 1..=12 {
+        println!("On the {day} day of christmas...");
+    }
 }
