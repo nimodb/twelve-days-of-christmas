6789ead8c36e48273812e2bda6829778936b0fd4
Nima Saadati <nimodbdev@gmail.com>
2024-08-26 12:59:46 +0400

    Modify gift printing function to handle the repetition for each day

--- src/main.rs
+++ src/main.rs
@@ -33,19 +33,27 @@ fn get_ordinal(day: u8) -> &'static str {
 }
 
 fn print_gifts(day: u8) {
-    match day {
-        12 => println!("12 Drummers Drumming"),
-        11 => println!("11 Pipers Piping"),
-        10 => println!("10 Lords a Leaping"),
-        9 => println!("9 Ladies Dancing"),
-        8 => println!("8 Maids a Milking"),
-        7 => println!("7 Swans a Swimming"),
-        6 => println!("6 Geese a Laying"),
-        5 => println!("5 Golden Rings"),
-        4 => println!("4 Calling Birds"),
-        3 => println!("3 French Hens"),
-        2 => println!("2 Turtle Doves"),
-        1 => println!("A Partridge in a Pear Tree"),
-        _ => (),
+    for current_day in (1..=day).rev() {
+        match current_day {
+            12 => println!("12 Drummers Drumming"),
+            11 => println!("11 Pipers Piping"),
+            10 => println!("10 Lords a Leaping"),
+            9 => println!("9 Ladies Dancing"),
+            8 => println!("8 Maids a Milking"),
+            7 => println!("7 Swans a Swimming"),
+            6 => println!("6 Geese a Laying"),
+            5 => println!("5 Golden Rings"),
+            4 => println!("4 Calling Birds"),
+            3 => println!("3 French Hens"),
+            2 => println!("2 Turtle Doves"),
+            1 => {
+                if day == 1 {
+                    println!("A Partridge in a Pear Tree");
+                } else {
+                    println!("Add a Partridge in a Pear Tree");
+                }
+            }
+            _ => (),
+        }
     }
 }
