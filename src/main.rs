a66a9733223af19c95e53319a7a264d5822a05b3
Nima Saadati <nimodbdev@gmail.com>
2024-08-26 12:51:00 +0400

    Add function to get ordinal representation for each day

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ fn main() {
 
 fn print_lyrics() {
     for day in 1..=12 {
-        println!("On the {day} day of christmas...");
+        println!("On the {} day of christmas...", get_ordinal(day));
+    }
+}
+
+fn get_ordinal(day: u8) -> &'static str {
+    match day {
+        1 => "first",
+        2 => "second",
+        3 => "third",
+        4 => "fourth",
+        5 => "fifth",
+        6 => "sixth",
+        7 => "seventh",
+        8 => "eighth",
+        9 => "ninth",
+        10 => "tenth",
+        11 => "eleventh",
+        12 => "twelfth",
+        _ => "",
     }
 }
